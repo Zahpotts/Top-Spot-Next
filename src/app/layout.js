922cfcd7a18db1588d8d1b3ae7cba945a5bf952f
Isaiah <zahpotts123@gmail.com>
2025-05-06 18:08:47 -0400

    fixed script issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,10 +19,11 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <div id = 'root'>{children}</div>
-        <script 
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" 
+        <Script 
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
           integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" 
           crossorigin="anonymous"
+          strategy="afterInteractive"
         />
       </body>
     </html>
